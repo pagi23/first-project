d815538c6126d1843d59acdfd0549ff7619ab29f
pagi23 <j.v.hodenberg@protonmail.com>
2017-09-23 16:01:02 +0200

    original state

---
+++ friendsProject/adresstest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        cout << "content-type: text/html" << endl << endl;
+        cout << "<h1>hallo stefan :)</h1>" << endl;
+        return 0;
+}
